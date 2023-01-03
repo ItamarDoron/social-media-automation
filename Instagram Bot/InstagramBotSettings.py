e8d804deb2003c3437257a12b51ed2305e321ca9
ItamarDoron <120119279+ItamarDoron@users.noreply.github.com>
2023-01-03 17:16:18 +0200

    Create InstagramBotSettings.py

---
+++ Instagram Bot/InstagramBotSettings.py
@@ -0,0 +1,32 @@
+
+# Paths for all buttons and input fields required for the "Instagram Bot"
+URL = "instagram.com"
+
+# home
+home_button_xpath = '//*[@aria-label="Home"]'
+
+# user look up
+search_button_xpath = '//*[@aria-label="Search"]'
+search_text_box_xpath = '//input[@placeholder="Search"]'
+
+# follow
+follow_button_xpath = '//div[text()="Follow"]'
+
+# messages
+message_button_xpath = '//div[text()="Message"]'
+message_textbox_xpath = '//textarea[@placeholder="Message..."]'
+send_button_xpath = '//button[text()="Send"]'
+message_wait_time = 5
+
+# user log-in
+username_path = 'username'
+password_path = 'password'
+log_in_button_xpath = '//div[text()="Log in"]'
+turn_on_notifications_pop_up_xpath = '//button[text()="Not Now"]'
+
+# user log-out
+more_options_button_xpath = '//*[@aria-label="Settings"]'
+log_out_button_xpath = '//div[text()="Log out"]'
+
+# password length
+minimum_password_length = 6
