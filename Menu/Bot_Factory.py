7eaf89fadcddf90cb28e4873aecc8b72cda6efda
ItamarDoron <120119279+ItamarDoron@users.noreply.github.com>
2023-02-17 14:14:54 +0200

    Update Bot_Factory.py

--- Menu/Bot_Factory.py
+++ Menu/Bot_Factory.py
@@ -1,27 +1,23 @@
-import InstagramBot
+import InstagramBot.InstagramBot
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import MenuSettings
-import BotMenu
+
+import MainBot.MainBot
+from Main_Bot_Menu import Browser_Config
 
 
 class Bot_Maker:
 
     @staticmethod
     def create_browser(browser_type):
-        type = browser_type.lower
-        if type == "chrome":
-            s = Service(MenuSettings.browsers[browser_type])
-            browser = webdriver.Chrome(service=s)
-            return browser
+        if browser_type.lower() == "chrome":
+            driver = webdriver.Chrome(service=Service(Browser_Config.browsers[browser_type]))
+            return driver
+
         # possible extension to more browsers
         print("Incorrect browser type")
 
     @staticmethod
-    def create_bot(bot_type, browser):
-        bot_type = bot_type.lower
-        if bot_type == "instagram":
-            return InstagramBot(browser)
-        # possible extension to more platforms
-        print("Incorrect platform")
-
+    def create_bot(bot_type, browser_type):
+        driver = Bot_Maker.create_browser(browser_type)
+        return InstagramBot.InstagramBot.InstagramBot(driver)
