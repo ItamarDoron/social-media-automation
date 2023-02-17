4a527ebebe9a47d362c67e652c3947cb590264f9
ItamarDoron <120119279+ItamarDoron@users.noreply.github.com>
2023-02-17 14:12:36 +0200

    Update InstagramBot.py

--- Instagram Bot/InstagramBot.py
+++ Instagram Bot/InstagramBot.py
@@ -1,76 +1,81 @@
-from MainBot.MainBot import MainBot
-import InstagramBotSettings
+import MainBot.MainBot
+from SettingFiles import InstagramBotSettings, MainBotSettings
 import selenium
-from selenium import webdriver
 from selenium.webdriver import Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from MainBot import MainBotSettings
 
-class InstagramBot(MainBot):
 
-    def __init__(self, browser): 
-        super().__init__(browser, InstagramBotSettings.URL)
+class InstagramBot(MainBot.MainBot.MainBot):
+
+    # TODO: Add hadle to both notifications and to login pop ups
+
+    def __init__(self, driver):
+        super().__init__(driver, InstagramBotSettings.URL)
         self.setup_login()
+        # Dictionary of actions a user can execute
         self.action_list = {
-            ("Follow user list", self.follow_user_list),
-            ("Message user list", self.message_user_list),
-            ("Follow user list", self.follow_user_list)
+            "Follow user list": self.follow_user_list,
+            "Message user list": self.message_user_list
         }
 
-    def show_actions(self):
-        for action in self.action_list.items():
-            print("{} - {}".format(action[0], action[1][0]))
+    def start(self):
+        super(InstagramBot, self).start()
 
-    def pick_action(self):
-        action = input("Enter value ({}-{}):".format(0, (len(self.action_list) - 1)))
-        return action
+        # log in to the account
 
-    def get_valid_action(self):
-        action = self.pick_action()
-        while not self.is_valid_action(action):
-            action = self.pick_action()
-        return action
-
-    def is_valid_action(self, action):
-        if action == MainBotSettings.quit_option:
-            return MainBotSettings.quit_option
-        if not action.isnumeric():
-            return False
-        action = int(action)
-        if action > len(self.action_list) - 1 or action < 0:
-            return False
-        return True
-
-    def take_action(self, action):
-        self.action_list[action][1]()
-
-    # log in to the account
     def setup_login(self):
         # get credentials
         username, password = self.getCredentials()
+        self.start()
+        self.wait_long()
         # create the buttons
-        log_in_button = self.browser.find_element(By.XPATH, InstagramBotSettings.log_in_button_xpath)  
-        username_field = self.browser.find_element(By.NAME, InstagramBotSettings.username_path)
-        password_field = self.browser.find_element(By.NAME, InstagramBotSettings.password_path)
+        log_in_button = self.driver.find_element(By.XPATH, InstagramBotSettings.log_in_button_xpath)
+        username_field = self.driver.find_element(By.NAME, InstagramBotSettings.username_path)
+        password_field = self.driver.find_element(By.NAME, InstagramBotSettings.password_path)
+        # preform the login
         self.login(username, password, password_field, username_field, log_in_button)
 
     def login(self, username, password, password_field, username_field, login_button):
         self.write_action(username, username_field)
         self.write_action(password, password_field)
         self.click_action(login_button)
+        self.wait_long()
         self.handle_notifications_window()
+        self.handle_notifications_window()
+
+    def pick_action(self):
+        self.show_actions()
+        last_sequence = len(self.action_list) - 1
+        try:
+            input_str = input("Enter value ({}-{}): ".format(0, last_sequence))
+            if input_str.lower() == 'q':
+                return MainBotSettings.quit_option
+            action = int(input_str)
+            if action not in range(len(self.action_list)):
+                raise ValueError("Action value out of range.")
+            return list(self.action_list.keys())[action]
+        except (ValueError, IndexError):
+            print("Invalid input. Please try again.")
+            return self.pick_action()
+
+    def show_actions(self):
+        for i, (key, value) in enumerate(self.action_list.items()):
+            print("{} - {}".format(i, key))
+        print("Enter " + MainBotSettings.quit_option + " to quit")
+
+    def take_action(self, key):
+        self.action_list[key]()
 
     def handle_notifications_window(self):
         # If notifications are off, instagram will ask to turn them on
-        turn_on_notifications_pop_up = self.browser.find_element(By.XPATH, InstagramBotSettings.turn_on_notifications_pop_up_xpath)
         try:
+            turn_on_notifications_pop_up = self.driver.find_element(By.XPATH, InstagramBotSettings.turn_on_notifications_pop_up_xpath)
             self.click_action(turn_on_notifications_pop_up)
         except selenium.common.exceptions.NoSuchWindowException:
             pass
 
     def getCredentials(self):
-        username = (input("Enter username: ")) 
+        username = (input("Enter username: "))  # TODO settings
         password = (input("Enter password: "))
         while len(password) < InstagramBotSettings.minimum_password_length:
             print("Password too short, must be " + InstagramBotSettings.minimum_password_length)
@@ -79,51 +84,64 @@ class InstagramBot(MainBot):
 
     # navigate to the home page
     def home(self):
-        home_button = self.browser.find_element(By.XPATH, InstagramBotSettings.home_button_xpath)
-        MainBot.click_action(home_button)
+        home_button = self.driver.find_element(By.XPATH, InstagramBotSettings.home_button_xpath)
+        self.click_action(home_button)
 
     # follow a certain user
     def follow(self, username):
         # go to the user we want to follow
-        self.userlookup(username)
+        self.user_look_up(username)
         # create the button
-        follow_button = self.browser.find_element(By.XPATH, InstagramBotSettings.follow_button_xpath)
+        follow_button = self.driver.find_element(By.XPATH, InstagramBotSettings.follow_button_xpath)
         # action
-        MainBot.click_action(follow_button)
+        self.click_action(follow_button)
 
     # go to a user's profile
     def user_look_up(self, username):
         # navigating to starting point (home page)
         self.home()
         # button making
-        search_button = self.browser.find_element(By.XPATH, InstagramBotSettings.search_button_xpath)
-        search_text_box = self.browser.find_element(By.XPATH, InstagramBotSettings.search_button_xpath)
+        search_button = self.driver.find_element(By.XPATH, InstagramBotSettings.search_button_xpath)
         # actions
-        MainBot.click_action(search_button)
-        MainBot.write_action(username, search_text_box)
+        self.click_action(search_button)
+        search_text_box = self.driver.find_element(By.XPATH, InstagramBotSettings.search_text_box_xpath)
+        self.write_action(username, search_text_box)
         # 2x enter for sending the search request
-        MainBot.write_action(Keys.ENTER, MainBot._short_action_time())
-        MainBot.write_action(Keys.ENTER, MainBot._short_action_time())
+        self.write_action(Keys.ENTER, search_text_box, self._short_action_time())
+        self.write_action(Keys.ENTER, search_text_box,  self._short_action_time())
 
     # message a certain user
     def message_user(self, username, text):
         # go to the user we want to message
-        self.userlookup(username)
-        # create the buttons
-        message_button = self.browser.find_element(By.XPATH, InstagramBotSettings.message_button_xpath)
-        message_textbox = self.browser.find_element(By.XPATH, InstagramBotSettings.message_textbox_xpath)
-        send_button = self.browser.find_element(By.XPATH, InstagramBotSettings.send_button_xpath)
-        # actions
+        self.user_look_up(username)
+        # go to message inbox
+        message_button = self.driver.find_element(By.XPATH, InstagramBotSettings.message_button_xpath)
         self.click_action(message_button, InstagramBotSettings.message_wait_time)
+        # enter message
+        message_textbox = self.driver.find_element(By.XPATH, InstagramBotSettings.message_textbox_xpath)
         self.write_action(text, message_textbox)
+        # send message
+        send_button = self.driver.find_element(By.XPATH, InstagramBotSettings.send_button_xpath)
         self.click_action(send_button)
 
     # mass message a user list a certain message
-    def message_user_list(self, user_list, text):
-        for username in user_list:
+    def message_user_list(self):
+        message_list = InstagramBot.get_user_list()
+        text = input(InstagramBotSettings.MESSAGE_INPUT)
+        for username in message_list:
             self.message_user(username, text)
 
     # mass follow a user list
-    def follow_user_list(self, user_list):
-        for username in user_list:
+    def follow_user_list(self):
+        follow_list = InstagramBot.get_user_list()
+        for username in follow_list:
             self.follow(username)
+
+    @staticmethod
+    def get_user_list():
+        user_list = []
+        user_input = input(InstagramBotSettings.USER_LIST_INPUT_MESSAGE)
+        if user_input:
+            user_list = user_input.split(",")
+            user_list = [username.strip() for username in user_list]
+        return user_list
