be614832df5999422f0fa0fb4cc853aec775c2df
ItamarDoron <120119279+ItamarDoron@users.noreply.github.com>
2023-02-17 14:18:32 +0200

    Create MenuSettings.py

---
+++ Menu/MenuSettings.py
@@ -0,0 +1,7 @@
+PICK_BROWSER_MESSAGE = "Please pick a browser from your setup configurations list\n"
+
+CONFIG_ERROR = "You must enter at least one browser in the config file in order to use\n"
+
+INVALID_ANSWER = "Invalid answer, please enter a number in the range: "
+
+PICK_PLATFORM_MESSAGE = "Please pick the platform\n"
